c1051998c8ffd48b858b3f585229089f6d4e6232
Wilson Vasquez <wilsonraymundov@gmail.com>
2023-05-05 06:34:34 -0600

    Primer Commit

---
+++ src/CAJERO_DB.java
@@ -0,0 +1,31 @@
+
+import INTERFACES.LOGIN;
+import java.awt.Image;
+import java.awt.Toolkit;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ale
+ */
+public class CAJERO_DB{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       
+        new LOGIN().setVisible(true);
+        
+     
+       
+        }
+      
+    }
